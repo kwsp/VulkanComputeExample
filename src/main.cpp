78dc22b4cafd6b7eb9d1882e0e9ec88a786ad414
tnie <tnie@tuta.io>
2024-11-01 15:42:52 -0500

    adding vulkan-memory-allocator

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,24 @@ int main(int argc, char *argv[]) {
 
   vcm::VulkanComputeManager manager;
 
+  {
+    const uint32_t N = 10;
+    const uint32_t bufferSize = N * sizeof(int32_t);
+
+    // Creating the buffers
+    vk::BufferCreateInfo bufCreateInfo{vk::BufferCreateFlags(), bufferSize,
+                                       vk::BufferUsageFlagBits::eStorageBuffer,
+                                       vk::SharingMode::eExclusive};
+
+    auto inBuffer = manager.get_device()->createBuffer(bufCreateInfo);
+    auto outBuffer = manager.get_device()->createBuffer(bufCreateInfo);
+
+    // Allocating memory
+    auto inBufferMemRequirements =
+        manager.get_device()->getBufferMemoryRequirements(inBuffer);
+    auto outBufferMemRequirements =
+        manager.get_device()->getBufferMemoryRequirements(outBuffer);
+  }
+
   return 0;
 }
