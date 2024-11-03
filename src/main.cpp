5edc978de068d8eecff5184122d631ffb9388f95
tnie <tnie@tuta.io>
2024-11-03 17:37:41 -0600

    added cmake infrastructure to compile shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "vcm/VulkanComputeManager.hpp"
 #include "vulkan/vulkan_core.h"
 #include <fmt/core.h>
+#include <fstream>
+#include <stdexcept>
 #include <vulkan/vulkan.hpp>
 
 int main(int argc, char *argv[]) {
@@ -21,6 +23,27 @@ int main(int argc, char *argv[]) {
 
     vcm::VcmBuffer inBuffer(manager.get_allocator(), bufCreateInfo, allocInfo);
     vcm::VcmBuffer outBuffer(manager.get_allocator(), bufCreateInfo, allocInfo);
+
+    // Load shader
+    std::vector<char> shaderContents;
+    const char *shaderFileName = "shaders/square.spv";
+    if (std::ifstream shaderFile{shaderFileName,
+                                 std::ios::binary | std::ios::ate}) {
+      const size_t fileSize = shaderFile.tellg();
+      shaderFile.seekg(0);
+      shaderContents.resize(fileSize, '\0');
+      shaderFile.read(shaderContents.data(), fileSize);
+    } else {
+      throw std::runtime_error(
+          fmt::format("Shader object file {} not found.", shaderFileName));
+    }
+
+    vk::ShaderModuleCreateInfo shaderModuleCreateInfo(
+        vk::ShaderModuleCreateFlags(),                              // Flags
+        shaderContents.size(),                                      // Code size
+        reinterpret_cast<const uint32_t *>(shaderContents.data())); // Code
+    vk::ShaderModule ShaderModule =
+        manager.get_device().createShaderModule(shaderModuleCreateInfo);
 
     inBuffer.destroy(manager.get_allocator());
     outBuffer.destroy(manager.get_allocator());
